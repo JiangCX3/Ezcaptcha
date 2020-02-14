07d4622ddb297efc78ec68c21b3c166aa72d2022
JiangCX3 <lazyfox158@outlook.com>
2020-02-14 23:56:07 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 #!/usr/bin/env python
 # coding: utf-8
+import json
 
 from setuptools import setup
 
+version = "0.0.1"
+tag = "-alpha"
+
+f = open("version.json", "w").write(json.dumps({
+    "version": version,
+    "tag": tag
+}))
+
 setup(
     name='ezcaptcha',
-    version='0.0.1',
+    version=version,
     author='JiangCX',
     author_email='jcx03@outlook.com',
     url='https://github.com/JiangCX3/Ezcaptcha',
